d8f2bb136d51fd27cd993c4ca338f76fa03a4a2e
sentient07 <vxrram95@gmail.com>
2014-06-12 02:35:51 +0530

    First Commit

---
+++ example.js
@@ -0,0 +1,21 @@
+angular.module('plunker', ['ui.bootstrap']);
+function TypeaheadCtrl($scope, $http) {
+
+  // Any function returning a promise object can be used to load values asynchronously
+  $scope.getLocation = function(val) {
+    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: val,
+        sensor: false
+      }
+    }).then(function(res){
+      var addresses = [];
+      angular.forEach(res.data.results, function(item){
+        addresses.push(item.formatted_address);
+      });
+      return addresses;
+    });
+  };
+
+ 
+}
